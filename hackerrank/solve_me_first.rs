2fac6513a358bdf2d4eff7123ce9704b8a5be78a
Ismail Rohaga <azh.zhafir@gmail.com>
2024-07-04 20:41:15 +0700

    feat: init hackerrank

---
+++ hackerrank/solve_me_first.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() {
+    let mut in0 = String::new();
+    let mut in1 = String::new();
+    
+    io::stdin().read_line(&mut in0).ok().expect("parse error");
+    io::stdin().read_line(&mut in1).ok().expect("parse error");
+    
+    let mut a: i32 = in0.trim().parse().ok().expect("parse error");
+    let mut b: i32 = in1.trim().parse().ok().expect("parse error");
+    
+    println!("{}", a + b)
+}

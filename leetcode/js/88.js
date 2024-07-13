a9f203a20e1de0c23baa74eedb0a2e442af61446
Ismail Rohaga <azh.zhafir@gmail.com>
2024-07-13 14:05:12 +0700

    feat: practice leetcode 88

---
+++ leetcode/js/88.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length);
+  nums2.splice(n, nums2.length);
+
+  nums1.push(...nums2);
+  nums1.sort();
+
+  return nums1;
+};
+
+var thesol = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+};
+
+console.log(merge([1, 2, 4, 5, 6, 0], 5, [3], 1));

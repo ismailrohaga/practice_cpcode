7bcfb395180740fa11dd66b6daaaea82def44731
Ismail Rohaga <azh.zhafir@gmail.com>
2024-07-24 10:33:52 +0700

    feat: practice leetcode 189

--- leetcode/js/189.js
+++ leetcode/js/189.js
@@ -4,10 +4,9 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function (nums, k) {
-  const first = nums.slice(0, k);
-  const second = nums.slice(k + 1, nums.length);
-  second.push(...first);
-  return second;
+  k = k % nums.length; // To handle cases where k is larger than the length of nums
+  const lastK = nums.splice(nums.length - k, k); // Extract the last k elements
+  return nums.unshift(...lastK);
 };
 
 console.log(rotate([1, 2, 3, 4, 5, 6, 7], 3));
